0c596d040bdb5288e72e876ff8cdec4871839ccc
Chu Nam Thắng <namthangmu123@gamil.com>
2022-04-22 00:01:38 +0700

    00:00 22/4/2022

--- app/src/main/java/vn/thanguit/toastperfect/MainActivity.java
+++ app/src/main/java/vn/thanguit/toastperfect/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ToastPerfect.makeText(this, ToastPerfect.SUCCESS, "OK em", Toast.LENGTH_SHORT).show();
+        ToastPerfect.makeText(this, ToastPerfect.SUCCESS, "Toast Perfect", Toast.LENGTH_SHORT).show();
     }
 }
